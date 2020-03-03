5af7ac51f00e750acaac3de048228b975aed331c
Lukas Bindeus <lbin@protonmail.com>
2020-03-03 11:16:54 +0100

    Remove old code

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
 import Vue from 'vue'
 import App from './src/App.vue'
 import('./pkg')
-  .then(rust_module => {
-    let fm = null;
-    window.getOscillator = () => new rust_module.FmOsc()
-    /*
-    const play_button = document.getElementById("play");
-    play_button.addEventListener("click", event => {
-      if (fm === null) {
-        fm = new rust_module.FmOsc();
-        fm.set_note(50);
-        fm.set_fm_frequency(0);
-        fm.set_fm_amount(0);
-        fm.set_gain(0.8);
-      } else {
-        fm.free();
-        fm = null;
-      }
-    });
-
-    const primary_slider = document.getElementById("primary_input");
-    primary_slider.addEventListener("input", event => {
-      if (fm) {
-        fm.set_note(parseInt(event.target.value));
-      }
-    });
-
-    const fm_freq = document.getElementById("fm_freq");
-    fm_freq.addEventListener("input", event => {
-      if (fm) {
-        fm.set_fm_frequency(parseFloat(event.target.value));
-      }
-    });
-
-    const fm_amount = document.getElementById("fm_amount");
-    fm_amount.addEventListener("input", event => {
-      if (fm) {
-        fm.set_fm_amount(parseFloat(event.target.value));
-      }
-    });
-    */
+  .then(rustModule => {
+    window.getOscillator = () => new rustModule.FmOsc()
   })
-  .catch(console.error);
+  .catch(console.error)
 
 new Vue({
   render: h => h(App)
