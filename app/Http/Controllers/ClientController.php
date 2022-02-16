7b600abc204551225937812a71a467f7d7d71d08
viruslight <61878631+viruslight@users.noreply.github.com>
2022-02-16 14:13:01 +0100

    add create and delete functions to ClientController (untested as of yet)

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,8 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Client;
 
 class ClientController extends Controller
 {
-    //
+  public function create(Request $request)
+  {
+    $client = new Client;
+    $client->token = create_token();
+    $client->performance_id = $request->performance_id;
+    $client->save();
+    return back()->with('flash', [
+        'message' => 'success',
+    ]);
+  }
+
+  public function delete(Request $request, $id) {
+    Client::where('id', $id)->delete();
+    return back()->with('flash', [
+      'message' => 'success',
+    ]);
+  }
+
+  public function create_token() {
+    // Make token unique? E.g. by making the field unique in db
+    return str_random(7);
+  }
 }
