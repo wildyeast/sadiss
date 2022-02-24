5d2c165b7d7dcca81a46c70076dcdbcbf715d0dd
wildyeast <32699708+wildyeast@users.noreply.github.com>
2022-02-24 13:07:30 +0100

    Prepare

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
-# wasm-fm-synth
+# sadiss-client 
 
-wasm-fm-synth will be a browser synthesizer/sequencer. The sound engine is
-taken from https://rustwasm.github.io/docs/wasm-bindgen/examples/web-audio.html
-
-To build this, [Rust](https://www.rust-lang.org/tools/install) and [wasm-pack](https://rustwasm.github.io/wasm-pack/installer/) have to be installed. Then run
-
-```
-$ yarn install
-$ yarn serve
-```
-
-The app will run on localhost:8080.
-
-You can use `yarn lint` to invoke eslint and `yarn fix` to autofix.
--- src/App.vue
+++ src/App.vue
@@ -1,170 +1,59 @@
 <template>
   <div class="app">
-    <div class="topBar">
-      <button
-        class="addLfo"
-        @click="toggleLinking"
-      >
-        🐸
-      </button>
-    </div>
-    <div class="modules">
-      <div class="oscillators">
-        <Oscillator
-          v-for="oscillator of modules.oscillators"
-          :key="`oscillator${oscillator.id}`"
-          :oscillator="oscillator"
-          :linking="linking"
-          @change="callOscillator(oscillator, $event)"
-          @addLfo="addLfo"
-        />
-      </div>
-      <div class="lfos">
-        <Lfo
-          v-for="lfo of modules.lfos"
-          :key="`lfo${lfo.id}`"
-          :lfo="lfo"
-          :linking="linking"
-          @addLfo="addLfo"
-        />
-      </div>
-    </div>
   </div>
 </template>
 <script>
 /* global getOscillator */
-import { modules, colors } from './constants'
-import Oscillator from './components/Oscillator.vue'
-import Lfo from './components/Lfo.vue'
+import { modules, bps } from './constants'
 
 export default {
   name: 'App',
-  components: { Oscillator, Lfo },
+  components: { },
   data: () => ({
     modules,
-    linking: false
+    linking: false,
+    osc: null
   }),
   async mounted () {
     // Wait until rust module loaded (see ../index.js)
     while (!window.getOscillator) {
       await this.sleep(100)
     }
+    // Initialises oscillator
     this.init()
+    // Just an example breakpoint loaded from ./constants
+    for (const bp of bps) {
+      await this.write(1, bp.freq, bp.amp)
+    }
+    // TODO Call the server api to fetch breakpoints instead.
   },
   methods: {
-    // Call wasm oscillator function
-    callOscillator (oscillator, key) {
-      const value = Number(oscillator[key].value)
-      const offset = Number(oscillator[key].offset)
-      const finalValue = value + offset
-      if (isNaN(finalValue)) {
-        console.error('Invalid oscillator input! key:', key, 'value:', value, 'offset:', offset, 'final:', finalValue)
-      }
-      oscillator.engine[`set_${key}`](finalValue)
-      oscillator[key].currentValue = finalValue
+    /* To give instructions to oscillator, use
+
+         oscillator.engine[command](value) 
+
+         Possible commands are:
+           'set_gain' to set amplitude 
+           'set_freq' to set frequency 
+         There are more (see ./lib.rs) but we will not need the others
+
+         Currently, oscillators are initialized with zero gain. 
+     */
+    async write (time, freq, gain) {
+      this.osc.engine['set_gain'](gain)
+      this.osc.engine['set_freq'](freq)
+      await this.sleep(10)
     },
+    /* Cheap js sleep function. Is not timing-accurate. We should try to
+      implement a timer as a Rust function as early as possible */
     async sleep (ms) {
-      // TODO Move timing mechanism to Rust for more accuracy
       await new Promise(resolve => setTimeout(resolve, ms))
     },
-    addLfo (targetContainer) {
-      if (!this.linking) return
-
-      let target
-      if (targetContainer.oscillator) { // Target is oscillator
-        target = targetContainer.oscillator[targetContainer.key]
-        target.oscillator = targetContainer.oscillator
-        target.key = targetContainer.key
-      } else { // Target is LFO
-        target = targetContainer
-      }
-
-      // Add indicator color
-      target.color = colors[modules.lfos.length]
-
-      this.linking = false
-      modules.lfos.push({
-        id: modules.lfos.length,
-        rate: {
-          value: 0,
-          offset: 0,
-          min: 0,
-          max: 1000,
-          step: 1,
-          currentValue: 0
-        },
-        color: colors[modules.lfos.length],
-        depth: {
-          value: 0,
-          offset: 0,
-          min: 0,
-          max: 1,
-          step: 0.1,
-          currentValue: 0
-        },
-        shape: 'sine',
-        target,
-        run: this.runLfo
-      })
-    },
-    toggleLinking () {
-      this.linking = !this.linking
-    },
-    async runLfo (lfo) {
-      let position = -1
-      let direction = 1
-      let sineHelperValue = 0
-      while (true) {
-        const depth = (lfo.target.max - lfo.target.min) * lfo.depth.currentValue
-        lfo.target.offset = depth * position
-        if (lfo.target.oscillator) { // target is oscillator
-          this.callOscillator(lfo.target.oscillator, lfo.target.key)
-        } else { // target is lfo
-          const value = Number(lfo.target.value)
-          const offset = Number(lfo.target.offset)
-          let finalValue = value + offset
-          if (finalValue < lfo.target.min) finalValue = lfo.target.min
-          if (finalValue > lfo.target.max) finalValue = lfo.target.max
-          lfo.target.currentValue = finalValue
-        }
-        switch (lfo.shape) {
-          case 'sine':
-            position = Math.sin(sineHelperValue)
-            sineHelperValue += Math.PI / 100
-            break
-          case 'square':
-            position = position * -1
-            break
-          case 'sawUp':
-            position += 0.01
-            if (position >= 1) position = -1
-            break
-          case 'sawDown':
-            if (position <= -1) position = 1
-            position -= 0.01
-            break
-          case 'triangle':
-            if (direction < 0) {
-              position -= 0.01
-            } else {
-              position += 0.01
-            }
-            if (position > 1 || position < -1) {
-              direction *= -1
-            }
-            break
-          case 'random':
-            position = Math.random() * 2 - 1
-            break
-          default:
-            console.error('Bad LFO shape:', lfo.shape)
-        }
-        await this.sleep(1000 - lfo.rate.currentValue)
-      }
-    },
+    /* Initialises oscillators. Should be 1 oscillator per partial */
     init () {
       for (const oscillator of modules.oscillators) {
         oscillator.engine = getOscillator()
+        this.osc = oscillator
         for (const key of Object.keys(oscillator)) {
           if (key === 'id') continue
           oscillator[key].value = oscillator[key].init
@@ -186,34 +75,5 @@ export default {
   flex-flow: column;
   align-items: center;
   overflow: hidden;
-}
-.topBar {
-  height: 5em;
-  background: black;
-  width: 100%;
-}
-.modules {
-  position: absolute;
-  top: 5em;
-  height: calc(100% - 5em);
-  overflow-y: scroll;
-}
-.oscillator, .lfo {
-  width: 98vw;
-}
-.label {
-  margin-top: 0.5em;
-  margin-left: 0.5em;
-}
-.addLfo {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  font-size: 4em;
-  cursor: pointer;
-  background: none;
-  border: none;
 }
 </style>
