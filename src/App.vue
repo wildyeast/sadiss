0a952346eb1b348693fadb10706bc5ed69b8058f
viruslight <61878631+viruslight@users.noreply.github.com>
2022-03-09 12:55:45 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -18,42 +18,15 @@ export default {
     osc: null,
     oscillators: [],
     partialData: null,
-    // allBreakpoints: [],
     wasm: null
   }),
   async mounted () {
-    // Wait until rust module loaded (see ../index.js)
-    // while (!window.getOscillator) {
-    //   await this.sleep(100)
-    // }
-
-    // this.wasm = await import('../pkg/index.js')
-
     // Fetch breakpoints from server
-    const res = await fetch ('http://8hz.at/api/track/8')
+    const res = await fetch ('http://8hz.at/api/track/3')
     const json = await res.json()
     this.partialData = JSON.parse(json.partials)
-    // const breakpoints = this.partialData[0].breakpoints
-
-    // this.audioContext = new(window.AudioContext || window.webkitAudioContext)()
-    // this.now = this.audioContext.currentTime;
-
-    // for (const partial of partialData) {
-    //   const osc = this.setupOscillator(partial, partial.startTime)
-    //   const oscObj = {
-    //     osc: osc.osc,
-    //     gain: osc.gain,
-    //     startTime: partial.startTime,
-    //     endTime: partial.endTime
-    //   }
-    //   this.oscillators.push(oscObj)
-    // }
-
-
-
   },
   methods: {
-
     setupOscillator(partial, startTime) {
       const osc = this.audioContext.createOscillator()
       const gain = this.audioContext.createGain()
@@ -68,41 +41,15 @@ export default {
       for (let i = 0; i < partial.breakpoints.length; i++) {
         const currentBreakpoint = partial.breakpoints[i]
         // if (times[i] - times[i-1] < 0) continue
-
         if (i > 0) {
           time += (currentBreakpoint.time - partial.breakpoints[i-1].time)
         }
-
         osc.frequency.setValueAtTime(currentBreakpoint.freq, time);
         gain.gain.setValueAtTime(currentBreakpoint.amp, time);
       }
 
       return {osc, gain}
 
-    },
-
-    async write (oscIndex, time, freq, gain) {
-      this.oscillators[oscIndex].engine['set_gain'](gain)
-      this.oscillators[oscIndex].engine['set_freq'](freq)
-      await this.sleep(time)
-    },
-    /* Cheap js sleep function. Is not timing-accurate. We should try to
-      implement a timer as a Rust function as early as possible */
-    async sleep (ms) {
-      await new Promise(resolve => setTimeout(resolve, ms))
-    },
-    /* Initialises oscillators. Should be 1 oscillator per partial */
-    init () {
-      // for (const oscillator of modules.oscillators) {
-        const oscillator = {...modules.oscillators[0]}
-        oscillator.engine = getOscillator()
-        // this.osc = oscillator
-        for (const key of Object.keys(oscillator)) {
-          if (key === 'id') continue
-          oscillator[key].value = oscillator[key].init
-        }
-        return oscillator
-      // }
     },
     async play () {
     // https://www.html5rocks.com/en/tutorials/audio/scheduling/
