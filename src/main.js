536f983e8bb64a3e9b3332eb320ed471cf708787
Mathias Bindeus <mbind@protonmail.com>
2022-04-09 18:11:53 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -3,27 +3,4 @@ import App from './App.vue'
 import './index.css'
 
 const app = createApp(App)
-
-// app.use(VueEcho, {
-//     broadcaster: 'pusher',
-//     key: 'laravel_rdb',
-//     cluster: 'mt1',
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-//     forceTLS: false,
-//     disableStats: true
-// })
-
 app.mount('#app')
-
-// import Echo from 'laravel-echo'
-// import Pusher from 'pusher-js'
-// window.Echo = new Echo({
-    // broadcaster: 'pusher',
-    // key: 'laravel_rdb',
-    // cluster: 'mt1',
-    // wsHost: window.location.hostname,
-    // wsPort: 6001,
-    // forceTLS: false,
-    // disableStats: true
-// });
