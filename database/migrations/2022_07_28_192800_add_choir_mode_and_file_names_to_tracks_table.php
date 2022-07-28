7ece1ea33e61ccdf7beefcfc2a9d77a778fbca8a
M <a@b.c>
2022-07-28 19:33:52 +0200

    Migration: Add is_choir, tts instruction file name, partials file name to DB

---
+++ database/migrations/2022_07_28_192800_add_choir_mode_and_file_names_to_tracks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            $table->string('tts_instuction_file_name');
+            $table->string('partials_file_name');
+            $table->boolean('is_choir')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            $table->dropColumn('tts_instructions');
+            $table->dropColumn('partials_file_name');
+            $table->dropColumn('is_choir');
+        });
+    }
+};
