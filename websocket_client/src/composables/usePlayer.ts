ca894bd81926d86198990e0dec3fd0bda38b9fce
Mathias <mbind@protonmail.com>
2022-12-14 22:59:23 +0100

    Minor

--- websocket_client/src/composables/usePlayer.ts
+++ websocket_client/src/composables/usePlayer.ts
@@ -6,6 +6,7 @@ export function usePlayer () {
   const oscillators: OscillatorObject[] = []
   let currentChunkStartTimeInCtxTime: number
   let startTime: number
+  let waitingForChunks = false
 
   const initialSetup = (partialChunks: PartialChunk[]) => {
     ctx = new AudioContext()
@@ -14,6 +15,7 @@ export function usePlayer () {
   }
 
   const handleChunkData = (partialChunks: PartialChunk[]) => {
+    waitingForChunks = false
     currentChunkStartTimeInCtxTime = startTime + partialChunks[0].startTime / 1000
     for (const chunk of partialChunks) {
       const oscObj = oscillators.find(el => el.index === chunk.index)
@@ -31,19 +33,20 @@ export function usePlayer () {
     }
   }
 
-  const startRequestChunksInterval = (ws: Ref<WebSocket | undefined>) => {
-    setInterval(() => {
-      if (currentChunkStartTimeInCtxTime <= ctx.currentTime) {
-        ws.value?.send(JSON.stringify('chunkRequest'))
-      }
-    }, 100)
-  }
-
   const setBreakpoints = (oscNode: OscillatorNode, breakpoints: Breakpoint[]) => {
     for (const bp of breakpoints) {
       oscNode.frequency.setValueAtTime(bp.freq, startTime + bp.time / 1000)
     }
     return oscNode
+  }
+
+  const startRequestChunksInterval = (ws: Ref<WebSocket | undefined>) => {
+    setInterval(() => {
+      if (currentChunkStartTimeInCtxTime <= ctx.currentTime && !waitingForChunks) {
+        ws.value?.send(JSON.stringify('chunkRequest'))
+        waitingForChunks = true
+      }
+    }, 10) // TODO: Intervals can probably be longer.
   }
 
   return {
