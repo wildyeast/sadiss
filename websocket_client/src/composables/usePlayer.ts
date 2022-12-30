749608d0121921ed65033f4ca1b665d0dcd010d1
Mathias <mbind@protonmail.com>
2022-12-30 21:06:20 +0100

    Improve Player

--- websocket_client/src/composables/usePlayer.ts
+++ websocket_client/src/composables/usePlayer.ts
@@ -21,7 +21,6 @@ export function usePlayer () {
 
   const startTimeInCtxTime = computed(() => globalStartTime - offset)
 
-  let numberOfChunksHandled = 0
   const MAXIMUM_CHUNK_LENGTH_S = 0.999
   const handleChunkData = (trackData: { partials: PartialChunk[], ttsInstructions: { startTime: number, text: string } }) => {
     console.log('\nHandling following chunk data: ', trackData)
@@ -32,18 +31,10 @@ export function usePlayer () {
     currentChunkStartTimeInCtxTime.value = startTimeInCtxTime.value
 
     for (const partialChunk of trackData.partials) {
-      console.log(oscillators.length, partialChunk.endTime, numberOfChunksHandled + MAXIMUM_CHUNK_LENGTH_S)
-      // if (oscillators.length && partialChunk.endTime === numberOfChunksHandled + MAXIMUM_CHUNK_LENGTH_S) {
-      if (oscillators.length && partialChunk.endTime >= numberOfChunksHandled + MAXIMUM_CHUNK_LENGTH_S) {
-        // Only assign new partialChunk to found oscillator if last breakpoint of partialChunk ends
-        // MAXIMUM_CHUNK_LENGTH_MS after chunk start.
-        // In this case we assume this is part of a longer partial and stitch the parts
-        // together seamlessly.
-        const oscObj = oscillators.find(el => el.index === partialChunk.index)
-        if (oscObj) {
-          console.log('Found osc.')
-          setBreakpoints(oscObj.oscillator, oscObj.gain, partialChunk.breakpoints, startTimeInCtxTime.value + partialChunk.endTime)
-        }
+      const oscObj = oscillators.find(el => el.index === partialChunk.index)
+      if (oscObj) {
+        console.log('Found osc.')
+        setBreakpoints(oscObj.oscillator, oscObj.gain, partialChunk.breakpoints, startTimeInCtxTime.value + partialChunk.endTime)
       } else {
         console.log('Creating osc.')
         const oscNode = ctx.createOscillator()
@@ -62,7 +53,6 @@ export function usePlayer () {
         oscillators.push({ index: partialChunk.index, oscillator: oscNode, gain: gainNode })
       }
     }
-    numberOfChunksHandled++
   }
 
   const setBreakpoints = (oscNode: OscillatorNode, gainNode: GainNode, breakpoints: Breakpoint[], chunkEndTime: number) => {
