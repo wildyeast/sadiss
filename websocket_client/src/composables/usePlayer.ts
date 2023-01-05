02dd143c8e5d6715b13a4660ae96785b1ba98bb3
Mathias <mbind@protonmail.com>
2023-01-05 20:32:17 +0100

    Fix: Remove unnecessary computed variable to enable playing back multiple tracks in one session

--- websocket_client/src/composables/usePlayer.ts
+++ websocket_client/src/composables/usePlayer.ts
@@ -5,7 +5,6 @@ export function usePlayer () {
   let ctx: AudioContext
   const oscillators: OscillatorObject[] = []
   const currentChunkStartTimeInCtxTime = ref()
-  let globalStartTime: number
   let offset: number
   let waitingForChunks = false
 
@@ -19,22 +18,21 @@ export function usePlayer () {
     offset = globalTime - ctx.currentTime
   }
 
-  const startTimeInCtxTime = computed(() => globalStartTime - offset)
+  // const startTimeInCtxTime = computed(() => globalStartTime - offset)
+  let startTimeInCtxTime: number
 
-  const MAXIMUM_CHUNK_LENGTH_S = 0.999
   const handleChunkData = (trackData: { partials: PartialChunk[], ttsInstructions: { startTime: number, text: string } }) => {
     console.log('\nHandling following chunk data: ', trackData)
 
-    // currentChunkStartTimeInCtxTime.value = startTimeInCtxTime.value + trackData.partials[0].startTime / 1000
-
     // TODO: This is not ideal, we shouldn't set globalStartTime every time we receive data
-    currentChunkStartTimeInCtxTime.value = startTimeInCtxTime.value
+    currentChunkStartTimeInCtxTime.value = startTimeInCtxTime
+
 
     for (const partialChunk of trackData.partials) {
       const oscObj = oscillators.find(el => el.index === partialChunk.index)
       if (oscObj) {
         console.log('Found osc.')
-        setBreakpoints(oscObj.oscillator, oscObj.gain, partialChunk.breakpoints, startTimeInCtxTime.value + partialChunk.endTime)
+        setBreakpoints(oscObj.oscillator, oscObj.gain, partialChunk.breakpoints, startTimeInCtxTime + partialChunk.endTime)
       } else {
         console.log('Creating osc.')
         const oscNode = ctx.createOscillator()
@@ -42,10 +40,11 @@ export function usePlayer () {
         oscNode.connect(gainNode)
         gainNode.connect(ctx.destination)
 
-        oscNode.start(startTimeInCtxTime.value + partialChunk.startTime)
-        setBreakpoints(oscNode, gainNode, partialChunk.breakpoints, startTimeInCtxTime.value + partialChunk.endTime)
+        oscNode.start(startTimeInCtxTime + partialChunk.startTime)
+        setBreakpoints(oscNode, gainNode, partialChunk.breakpoints, startTimeInCtxTime + partialChunk.endTime)
 
-        oscNode.stop(startTimeInCtxTime.value + partialChunk.endTime)
+        oscNode.stop(startTimeInCtxTime + partialChunk.endTime)
+
         oscNode.onended = (event) => {
           const oscObj = oscillators.find(oscObj => oscObj.oscillator === event.target)
           if (oscObj) oscillators.splice(oscillators.indexOf(oscObj), 1)
@@ -71,7 +70,7 @@ export function usePlayer () {
 
   const chunksRequested = () => waitingForChunks = true
 
-  const setStartTime = (startTime: number) => globalStartTime = startTime
+  const setStartTime = (startTime: number) => startTimeInCtxTime = startTime - offset
 
   return {
     chunksRequested,
