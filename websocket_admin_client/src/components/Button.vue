7040293df1997786fb72b2eea30fd6f3a6865026
wildyeast <lbin@protonmail.com>
2023-01-13 15:15:35 +0100

    Add components

---
+++ websocket_admin_client/src/components/Button.vue
@@ -0,0 +1,3 @@
+<template>
+  <button class="p-2 m-2 bg-white text-primary font-bold flex justify-center items-center rounded shadow"><slot /></button>
+</template>
---
+++ websocket_admin_client/src/components/Modal.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const props = defineProps(['title'])
+</script>
+<template>
+  <div class="absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-modal">
+    <div class="w-1/2 bg-white text-primary rounded shadow">
+      <div class="w-100 h-8 border-b border-primary flex flex-row justify-between items-center p-2 font-bold">
+        <div>{{ title }}</div>
+        <div @click="$emit('close')" class="cursor-pointer">✖</div>
+      </div>
+      <div><slot /></div>
+    </div>
+  </div>
+</template>
