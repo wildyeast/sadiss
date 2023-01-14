b136d85c301d2b34a54ac4d7bf6565715a30f429
Mathias <mbind@protonmail.com>
2023-01-14 17:33:03 +0100

    Autoformat admin-client HomeView.vue

--- websocket_admin_client/src/views/HomeView.vue
+++ websocket_admin_client/src/views/HomeView.vue
@@ -1,55 +1,62 @@
 <script setup lang="ts">
-  import { ref, onMounted, inject } from 'vue'
+import { ref, onMounted, inject } from 'vue'
 
-  const isUploadModalVisible = ref(false)
-  const axios: any = inject('axios')
+const isUploadModalVisible = ref(false)
+const axios: any = inject('axios')
 
-  const trackName = ref('')
-  const trackNotes = ref('')
-  let file
+const trackName = ref('')
+const trackNotes = ref('')
+let file
 
-  const handleFileUpload = async (e) => {
-    file = e.target.files[0]
+const handleFileUpload = async (e) => {
+  file = e.target.files[0]
+}
+
+const upload = () => {
+  if (!trackName.value) {
+    alert('You must enter a track title.')
   }
-
-  const upload = () => {
-    if (!trackName.value) {
-      alert('You must enter a track title.')
-    }
-    const data = new FormData()
-    data.append('pfile', file)
-    data.append('name', trackName.value)
-    data.append('notes', trackNotes.value)
-    axios.post('http://localhost:3000/upload', data)
+  const data = new FormData()
+  data.append('pfile', file)
+  data.append('name', trackName.value)
+  data.append('notes', trackNotes.value)
+  axios.post('http://localhost:3000/upload', data)
     .then(response => {
-          console.log(response.data)
-      })
-      .catch(error => {
-          console.log(error)
-      });
-  }
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    });
+}
 </script>
 <template>
   <div class="flex flex-col">
     <Button @click="isUploadModalVisible = true">Upload new track</Button>
-    <Modal title="Upload track" v-if="isUploadModalVisible">
+    <Modal title="Upload track"
+           v-if="isUploadModalVisible">
       <div class="flex flex-row my-8 justify-between p-2">
         <div>Title</div>
-        <input v-model="trackName" class="w-3/4">
+        <input v-model="trackName"
+               class="w-3/4">
       </div>
       <div class="flex flex-row my-8 justify-between p-2">
         <div>Partials file</div>
-        <input type="file" @change="handleFileUpload($event)"
-           accept="*.txt" class="w-3/4">
+        <input type="file"
+               @change="handleFileUpload($event)"
+               accept="*.txt"
+               class="w-3/4">
       </div>
       <div class="flex flex-row my-8 justify-between p-2">
         <div>Subtitle files</div>
-        <input type="file" @change="handleFileUpload($event)"
-           accept="*.txt" class="w-3/4">
+        <input type="file"
+               @change="handleFileUpload($event)"
+               accept="*.txt"
+               class="w-3/4">
       </div>
       <div class="flex flex-row my-8 justify-between p-2">
         <div>Private notes</div>
-        <textarea v-model="trackNotes" class="w-3/4" />
+        <textarea v-model="trackNotes"
+                  class="w-3/4" />
       </div>
       <div class="flex flex-row w-full justify-center">
         <Button @click="upload">Upload</Button>
