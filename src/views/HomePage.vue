45ae5fed6f8ff745101d4a246e3fffcb50e34bba
Mathias <mbind@protonmail.com>
2023-01-15 14:38:37 +0100

    Order logged partial ids asc

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -66,6 +66,7 @@ const VUE_APP_MCORP_API_KEY = '8844095860530063641'
 const VUE_APP_WS_SERVER_URL = '192.168.0.87'
 const VUE_APP_WS_SERVER_PORT = '443'
 
+const VUE_APP_WS_LIVE_SERVER_URL = 'ws://sadiss.net/ws'
 
 const isRegistered = ref(false)
 const ws = ref<WebSocket>()
@@ -89,6 +90,7 @@ const establishWebsocketConnection = () => {
   startAudioCtx(globalTime.value)
 
   ws.value = new WebSocket(`ws://${VUE_APP_WS_SERVER_URL}:${VUE_APP_WS_SERVER_PORT}`)
+  // ws.value = new WebSocket(VUE_APP_WS_LIVE_SERVER_URL)
 
   ws.value.onopen = function () {
     console.log('Connection is open')
@@ -119,7 +121,7 @@ const establishWebsocketConnection = () => {
     // TODO: This is not ideal, we shouldn't set globalStartTime every time we receive data
     setStartTime(data.startTime)
     if (Object.keys(data.chunk)) {
-      dLog('Partials: ' + data.chunk.partials.map((el: any) => el.index).join())
+      dLog('Partials: ' + data.chunk.partials.map((el: any) => el.index).sort((a: number, b: number) => a - b).join(', '))
       handleChunkData(data.chunk)
     }
   }
