340b779bb33eada7aa5a0a17b256fededef38525
Mathias <mbind@protonmail.com>
2023-02-08 19:21:59 +0100

    Make modal have fixed position

--- websocket_admin_client/src/components/Modal.vue
+++ websocket_admin_client/src/components/Modal.vue
@@ -2,7 +2,7 @@
 const props = defineProps(['title'])
 </script>
 <template>
-  <div class="absolute top-0 left-0 w-screen h-screen flex justify-center items-center bg-modal">
+  <div class="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-modal overflow-scroll">
     <div class="w-1/2 bg-white text-primary rounded shadow">
       <div class="w-100 h-8 border-b border-primary flex flex-row justify-between items-center p-2 font-bold">
         <div>{{ title }}</div>
