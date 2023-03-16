34e1757d714524a370c95b859d8c949ac1276b8c
Mathias <mbind@protonmail.com>
2023-03-16 15:19:55 +0100

    Improve readability

--- server/auth.ts
+++ server/auth.ts
@@ -34,7 +34,7 @@ passport.use(
 const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+  if (!token) return res.sendStatus(401)
 
   jwt.verify(token, process.env.JWT_SECRET!, (err: any, user: any) => {
     if (err) return res.sendStatus(403)
