9492796b4523fb651d8fd9d60c87118d293e4310
Mathias <mbind@protonmail.com>
2023-04-26 10:21:49 +0200

    Add footer-doots.svg; Add them to BasePage

--- app/src/components/BasePage.vue
+++ app/src/components/BasePage.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex min-h-full w-full flex-col items-center justify-between overflow-scroll bg-primary p-4 px-6">
+  <div class="flex h-full w-full flex-col justify-between overflow-scroll bg-primary">
     <div class="flex flex-col items-center">
       <img
         src="../../public/assets/sadiss-logo.png"
-        class="my-8 w-1/2" />
+        class="my-6 w-1/2" />
     </div>
-    <slot />
+    <main class="flex flex-grow flex-col items-center justify-between p-4 px-6">
+      <slot />
+    </main>
+    <img src="../../public/assets/footer-dots.svg" />
   </div>
 </template>
