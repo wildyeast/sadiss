eecedc44a4e3e7a83e9edefca3c4b1dd99848891
Mathias <mbind@protonmail.com>
2023-04-27 13:31:13 +0200

    Decrease bottom margin of logo in BasePage.vue

--- app/src/components/BasePage.vue
+++ app/src/components/BasePage.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-col items-center">
       <img
         src="../../public/assets/sadiss-logo.png"
-        class="my-6 w-1/2" />
+        class="mt-6 mb-2 w-1/2" />
     </div>
     <main class="flex flex-grow flex-col items-center justify-between p-4 px-6">
       <slot />
