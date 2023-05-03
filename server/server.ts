7515c9ec91c3bc6b3b6a1b4cda140ddbfa021196
Mathias <mbind@protonmail.com>
2023-05-03 22:17:22 +0200

    Disable cors

--- server/server.ts
+++ server/server.ts
@@ -52,7 +52,7 @@ const corsOptions = {
 
 const app = express()
   .use(express.json())
-  .use(cors(corsOptions))
+  // .use(cors(corsOptions))
   .use(express.urlencoded({ extended: false }))
 
 app.listen(BASE_PORT, () => console.log(`Http server listening on port ${BASE_PORT}.`))
