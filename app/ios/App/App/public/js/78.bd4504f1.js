c0dd28cc5a0818f4b19bb3ffd95e2db34192ddf2
Mathias <mbind@protonmail.com>
2023-05-05 15:30:21 +0200

    Commit built iOS files

--- app/ios/App/App/public/js/78.bd4504f1.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunksadiss_app"]=self["webpackChunksadiss_app"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return a}});var s=n(3087),r=n(8487),o=n(6587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.bd4504f1.js.map
--- app/ios/App/App/public/js/990.1f516387.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunksadiss_app"]=self["webpackChunksadiss_app"]||[]).push([[990],{8990:function(t,e,n){n.r(e),n.d(e,{createSwipeBackGesture:function(){return i}});var s=n(6587),r=n(545),a=n(6515);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const i=(t,e,n,i,o)=>{const c=t.ownerDocument.defaultView,u=(0,r.i)(t),d=t=>{const e=50,{startX:n}=t;return u?n>=c.innerWidth-e:n<=e},h=t=>u?-t.deltaX:t.deltaX,l=t=>u?-t.velocityX:t.velocityX,p=t=>d(t)&&e(),f=t=>{const e=h(t),n=e/c.innerWidth;i(n)},k=t=>{const e=h(t),n=c.innerWidth,r=e/n,a=l(t),i=n/2,u=a>=0&&(a>.2||e>i),d=u?1-r:r,p=d*n;let f=0;if(p>5){const t=p/Math.abs(a);f=Math.min(t,540)}o(u,r<=0?.01:(0,s.e)(0,r,.9999),f)};return(0,a.A)({el:t,gestureName:"goback-swipe",gesturePriority:40,threshold:10,canStart:p,onStart:n,onMove:f,onEnd:k})}}}]);
-//# sourceMappingURL=990.1f516387.js.map
