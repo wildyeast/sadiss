5f4329708ac8dcec24ee03b0fc95922937fc78e0
Mathias <mbind@protonmail.com>
2023-05-08 23:46:01 +0200

    Go to '/' when clicking on logo in top bar, make catch all path go to '/', admin_client

--- admin_client/src/components/TopBar.vue
+++ admin_client/src/components/TopBar.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts"></script>
 <template>
   <div class="flex h-[80px] w-full items-center border-b border-light px-2">
-    <img
-      src="https://sadiss.net/wp-content/uploads/2022/07/Sadiss-Logo.png"
-      class="h-12" />
+    <router-link to="/">
+      <img
+        src="https://sadiss.net/wp-content/uploads/2022/07/Sadiss-Logo.png"
+        class="h-12" />
+    </router-link>
     <nav v-if="false">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
