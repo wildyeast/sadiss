3b1d2930c47dcde1d6b7f4da7434251912bf56d2
Mathias <mbind@protonmail.com>
2023-05-26 23:03:59 +0200

    Minor layout improvements

--- admin_client/src/views/DashboardView.vue
+++ admin_client/src/views/DashboardView.vue
@@ -1,9 +1,17 @@
 <template>
   <main>
-    <h1>Dashboard</h1>
-    <div class="mt-8 flex flex-col items-center gap-4">
-      <router-link to="/performances">Performances</router-link>
-      <router-link to="/tracks">Tracks</router-link>
+    <h1 class="text-center text-3xl font-bold">Dashboard</h1>
+    <div class="mt-6 flex items-center gap-4 px-2">
+      <router-link
+        to="/performances"
+        class="w-1/2 border p-6 text-center text-lg"
+        >Performances
+      </router-link>
+      <router-link
+        to="/tracks"
+        class="w-1/2 border p-6 text-center text-lg"
+        >Tracks
+      </router-link>
     </div>
   </main>
 </template>
