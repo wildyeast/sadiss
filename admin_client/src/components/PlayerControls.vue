24b8d5a339a46501d8a4f1675e19e065ec80e676
Mathias <mbind@protonmail.com>
2023-05-26 08:48:17 +0200

    Move playback logic to PlayerControls.vue

--- admin_client/src/components/PlayerControls.vue
+++ admin_client/src/components/PlayerControls.vue
@@ -1,23 +1,45 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 import type { Track } from '@/types/types'
+import { startTrack, stopTrack } from '@/services/api'
 
 const props = defineProps<{
-  isPlaying: boolean
-  progress: number
-  currentChunkIndex: number
-  totalChunks: number
+  performanceId: string
   selectedTrack: Track
 }>()
 
-defineEmits<{
-  startTrack: () => void
-  stopTrack: () => void
-}>()
+let motion: any
+const globalTime = ref(-1)
+const initializeMCorp = async () => {
+  // @ts-ignore: Can't find name MCorp, which is added via <script> in index.html
+  const mCorpApp = MCorp.app(import.meta.env.VITE_APP_MCORP_API_KEY, { anon: true })
+  mCorpApp.run = () => {
+    motion = mCorpApp.motions['shared']
+    startClock()
+  }
+  mCorpApp.init()
+}
+
+const startClock = () => {
+  setInterval(() => {
+    globalTime.value = motion.pos
+  }, 10)
+}
+
+const playingTrackId = ref<string>('')
+
+const handleStartTrack = async () => {
+  await startTrack(props.selectedTrack._id, props.performanceId, globalTime.value, shouldLoop.value)
+  playingTrackId.value = props.selectedTrack._id
+}
+
+const handleStopTrack = async () => {
+  await stopTrack(props.performanceId)
+}
 
 const shouldLoop = ref(false)
 const toggleLoop = () => {
-  if (props.isPlaying) {
+  if (playingTrackId.value) {
     alert('Cannot toggle loop while track is playing')
     return
   }
@@ -28,6 +50,77 @@ const shouldGoToNextTrack = ref(false)
 const toggleShouldGoToNextTrack = () => {
   shouldGoToNextTrack.value = !shouldGoToNextTrack.value
 }
+
+const wsUrl = import.meta.env.VITE_APP_WS_URL
+let attemptingToRegister = false
+const isRegistered = ref(false)
+const ws = ref<WebSocket>()
+
+const progress = ref<number>(0)
+const currentChunkIndex = ref<number>(0)
+const totalChunks = ref<number>(0)
+
+const establishWebsocketConnection = async () => {
+  if (attemptingToRegister || isRegistered.value) return
+  attemptingToRegister = true
+  ws.value = new WebSocket(wsUrl)
+
+  ws.value.onopen = function () {
+    isRegistered.value = true
+    attemptingToRegister = false
+    this.send(JSON.stringify({ message: 'isAdmin', performanceId: props.performanceId }))
+  }
+
+  ws.value.onclose = () => {
+    isRegistered.value = false
+    attemptingToRegister = false
+    // TODO: Handle lost connection. Does not mean that playback has stopped.
+  }
+
+  ws.value.onerror = (error) => {
+    isRegistered.value = false
+    attemptingToRegister = false
+    console.error(JSON.stringify(error))
+    // TODO: Handle error
+  }
+
+  ws.value.onmessage = (event) => {
+    if (!event.data) {
+      return
+    }
+
+    const data = JSON.parse(event.data)
+
+    if (data.start) {
+      // TODO: Handle start
+    }
+
+    if (data.stop) {
+      // TODO: Handle stop
+      playingTrackId.value = ''
+      progress.value = 0
+      currentChunkIndex.value = 0
+      totalChunks.value = 0
+      return
+    }
+
+    if (data.trackId) {
+      playingTrackId.value = data.trackId
+      progress.value = Math.floor((data.chunkIndex / data.totalChunks) * 100)
+      currentChunkIndex.value = data.chunkIndex
+      totalChunks.value = data.totalChunks
+    }
+  }
+}
+
+onMounted(async () => {
+  initializeMCorp()
+  establishWebsocketConnection()
+})
+
+onUnmounted(() => {
+  ws.value?.close()
+})
 </script>
 
 <template>
@@ -44,15 +137,15 @@ const toggleShouldGoToNextTrack = () => {
           size="lg" />
       </button>
       <button
-        v-if="!isPlaying"
-        @click="$emit('startTrack')">
+        v-if="!playingTrackId"
+        @click="handleStartTrack">
         <font-awesome-icon
           icon="fa-play-circle"
           size="2x" />
       </button>
       <button
         v-else
-        @click="$emit('stopTrack')">
+        @click="handleStopTrack">
         <font-awesome-icon
           icon="fa-stop-circle"
           size="2x" />
--- admin_client/src/views/PerformanceView.vue
+++ admin_client/src/views/PerformanceView.vue
@@ -10,109 +10,11 @@ const route = useRoute()
 const performanceId = route.params.id
 const performance = ref<SadissPerformance>()
 
-const handleStartTrack = async () => {
-  if (!selectedTrack.value?._id) {
-    alert('No track selected')
-    return
-  }
-  await startTrack(selectedTrack.value?._id, performanceId as string, globalTime.value)
-  playingTrackId.value = selectedTrack.value?._id
-}
-
-const handleStopTrack = async () => {
-  await stopTrack(performanceId as string)
-}
-
-let motion: any
-const globalTime = ref(-1)
-const initializeMCorp = async () => {
-  // @ts-ignore: Can't find name MCorp, which is added via <script> in index.html
-  const mCorpApp = MCorp.app(import.meta.env.VITE_APP_MCORP_API_KEY, { anon: true })
-  mCorpApp.run = () => {
-    motion = mCorpApp.motions['shared']
-    startClock()
-  }
-  mCorpApp.init()
-}
-
-const startClock = () => {
-  setInterval(() => {
-    globalTime.value = motion.pos
-  }, 10)
-}
-
-const wsUrl = import.meta.env.VITE_APP_WS_URL
-let attemptingToRegister = false
-const isRegistered = ref(false)
-const ws = ref<WebSocket>()
-
 const selectedTrack = ref<Track>()
 const playingTrackId = ref<string>('')
-const playingTrackProgress = ref<number>(0)
-const playingTrackCurrentChunkIndex = ref<number>(0)
-const playingTrackTotalChunks = ref<number>(0)
-
-const establishWebsocketConnection = async () => {
-  if (attemptingToRegister || isRegistered.value) return
-  attemptingToRegister = true
-  ws.value = new WebSocket(wsUrl)
-
-  ws.value.onopen = function () {
-    isRegistered.value = true
-    attemptingToRegister = false
-    this.send(JSON.stringify({ message: 'isAdmin', performanceId }))
-  }
-
-  ws.value.onclose = () => {
-    isRegistered.value = false
-    attemptingToRegister = false
-    // TODO: Handle lost connection. Does not mean that playback has stopped.
-  }
-
-  ws.value.onerror = (error) => {
-    isRegistered.value = false
-    attemptingToRegister = false
-    console.error(JSON.stringify(error))
-    // TODO: Handle error
-  }
-
-  ws.value.onmessage = (event) => {
-    if (!event.data) {
-      return
-    }
-
-    const data = JSON.parse(event.data)
-
-    if (data.start) {
-      // TODO: Handle start
-    }
-
-    if (data.stop) {
-      // TODO: Handle stop
-      playingTrackId.value = ''
-      playingTrackProgress.value = 0
-      playingTrackCurrentChunkIndex.value = 0
-      playingTrackTotalChunks.value = 0
-      return
-    }
-
-    if (data.trackId) {
-      playingTrackId.value = data.trackId
-      playingTrackProgress.value = Math.floor((data.chunkIndex / data.totalChunks) * 100)
-      playingTrackCurrentChunkIndex.value = data.chunkIndex
-      playingTrackTotalChunks.value = data.totalChunks
-    }
-  }
-}
 
 onMounted(async () => {
   performance.value = await getPerformanceWithTracks(performanceId as string)
-  initializeMCorp()
-  establishWebsocketConnection()
-})
-
-onUnmounted(() => {
-  ws.value?.close()
 })
 </script>
 <template>
@@ -128,30 +30,11 @@ onUnmounted(() => {
         class="relative flex w-full justify-between border p-4"
         :class="{ 'bg-secondary': selectedTrack === track }">
         <p>{{ track.name }}</p>
-        <button
-          v-if="track._id !== playingTrackId"
-          @click.stop="">
-          <font-awesome-icon
-            icon="fa-trash"
-            size="lg" />
-        </button>
-        <button
-          v-else
-          @click.stop="handleStopTrack">
-          <font-awesome-icon
-            icon="fa-pause"
-            size="lg" />
-        </button>
       </button>
     </div>
     <PlayerControls
       v-if="selectedTrack"
-      :isPlaying="playingTrackId !== ''"
-      :progress="playingTrackProgress"
-      :currentChunkIndex="playingTrackCurrentChunkIndex"
-      :totalChunks="playingTrackTotalChunks"
       :selectedTrack="selectedTrack"
-      @start-track="handleStartTrack"
-      @stop-track="handleStopTrack" />
+      :performance-id="(performanceId as string)" />
   </main>
 </template>
