74a73e964352cb50434b02bf47b33a6918ffa4cd
wildyeast <32699708+wildyeast@users.noreply.github.com>
2023-06-30 18:32:01 +0000

    Fix typo

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -15,7 +15,7 @@ const router = express.Router()
 
 import multer from 'multer'
 
-const upload = multer({ dest: 'upload/' });
+const upload = multer({ dest: 'uploads/' });
 
 router.use('/api', authenticateToken)
 
