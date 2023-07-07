fcabd5859041041e9ec0eae0727452b2e5737c06
Mathias <mbind@protonmail.com>
2023-07-07 09:32:54 +0200

    Fix test file name, work on choir mode tests

--- server/tests/activePerformance.spec.ts
+++ server/tests/activePerformance.spec.ts
@@ -15,7 +15,7 @@ describe('activePerformance test', () => {
       const ttsRate = '1'
 
       const { activePerformance, chunks } = preparePerformance(
-        'tests/testFiles/testChunkFile_shortTwoPartials.json',
+        'tests/testFiles/testChunkFile_shortOnePartial.json',
         trackMode,
         waveform,
         ttsRate
@@ -31,13 +31,13 @@ describe('activePerformance test', () => {
       expect(testClient.send).toHaveBeenNthCalledWith(1, JSON.stringify({ start: true }))
     })
 
-    it('should send partials and ttsInstructions to clients in the performance that are not admins', () => {
+    it('choir mode: should send partials and ttsInstructions to clients in the performance that are not admins', () => {
       const trackMode = 'choir'
       const waveform = 'sine'
       const ttsRate = '1'
 
       const { activePerformance, chunks } = preparePerformance(
-        'tests/testFiles/testChunkFile_shortTwoPartials.json',
+        'tests/testFiles/testChunkFile_shortOnePartial.json',
         trackMode,
         waveform,
         ttsRate
@@ -45,15 +45,21 @@ describe('activePerformance test', () => {
 
       // Add clients to wss
       const testClient = createMockWsClient(activePerformance.id, 0)
+      const testAdminClient = createMockWsClient(activePerformance.id, 0, true)
 
       // Start sending interval
       const startTime = 0
-      activePerformance.startSendingInterval(startTime, false, generateMockId())
-      jest.advanceTimersByTime(1000)
+      const mockTrackId = generateMockId()
+      const loopTrack = false
+      activePerformance.startSendingInterval(startTime, loopTrack, mockTrackId)
+
+      const sendingIntevalLength = 1000 // ms
+
+      jest.advanceTimersByTime(sendingIntevalLength)
 
       const TIME_ADDED_TO_START = 2 // seconds
 
-      const partialForTestClient = chunks[0]?.partials.find((chunk: PartialChunk) => chunk.index === testClient.choirId)
+      let partialForTestClient = chunks[0]?.partials.find((chunk: PartialChunk) => chunk.index === testClient.choirId)
       expect(testClient.send).toHaveBeenNthCalledWith(
         2,
         JSON.stringify({
@@ -61,6 +67,37 @@ describe('activePerformance test', () => {
           waveform,
           ttsRate,
           chunk: { partials: [partialForTestClient] }
+        })
+      )
+      expect(testAdminClient.send).toHaveBeenNthCalledWith(
+        2,
+        JSON.stringify({
+          chunkIndex: 0,
+          totalChunks: 2,
+          trackId: mockTrackId,
+          loop: loopTrack
+        })
+      )
+
+      jest.advanceTimersByTime(sendingIntevalLength)
+
+      partialForTestClient = chunks[1]?.partials.find((chunk: PartialChunk) => chunk.index === testClient.choirId)
+      expect(testClient.send).toHaveBeenNthCalledWith(
+        3,
+        JSON.stringify({
+          startTime: startTime + TIME_ADDED_TO_START,
+          waveform,
+          ttsRate,
+          chunk: { partials: [partialForTestClient] }
+        })
+      )
+      expect(testAdminClient.send).toHaveBeenNthCalledWith(
+        3,
+        JSON.stringify({
+          chunkIndex: 1,
+          totalChunks: 2,
+          trackId: mockTrackId,
+          loop: loopTrack
         })
       )
     })
