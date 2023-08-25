7bd354ffa5efd8bbc28d41159ecebab2ff6a3832
Mathias <mbind@protonmail.com>
2023-08-25 10:26:03 +0200

    admin_client/server: Remove changing of files when editing track as it doesn't work correctly at the moment

--- admin_client/src/components/AddTrackModal.vue
+++ admin_client/src/components/AddTrackModal.vue
@@ -198,7 +198,9 @@ watch(trackTtsRate, (newValue) => {
       <hr />
 
       <!-- Partial File Upload -->
-      <div class="lg:flex lg:flex-row">
+      <div
+        v-if="!isEditingTrack"
+        class="lg:flex lg:flex-row">
         <div class="flex justify-between gap-2">
           <div>Partials file</div>
           <input
@@ -236,9 +238,16 @@ watch(trackTtsRate, (newValue) => {
       </div>
 
       <hr />
+      <p
+        v-if="isEditingTrack"
+        class="text-primary">
+        Changing partial or TTS files is currently in development. To change files delete the track and create a new one.
+      </p>
 
       <!-- TTS (.srt or .txt) file upload -->
-      <div class="lg:flex lg:flex-col">
+      <div
+        v-if="!isEditingTrack"
+        class="lg:flex lg:flex-col">
         <div class="flex flex-col">
           <div>Subtitle files</div>
           <div>
