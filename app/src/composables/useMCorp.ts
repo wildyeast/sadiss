4a9fd6aafdae69410a0b1c834c0d649ac10a136e
Mathias <mbind@protonmail.com>
2023-09-11 15:09:18 +0200

    App: Use API key from .env file instead of hardcoding it; Add .env.example

--- app/src/composables/useMCorp.ts
+++ app/src/composables/useMCorp.ts
@@ -3,13 +3,11 @@ import { usePlayer } from './usePlayer'
 
 const { setMotionRef } = usePlayer()
 
-const VUE_APP_MCORP_API_KEY = '8844095860530063641'
-
 export function useMCorp() {
   const initializeMCorp = async () => {
     // @ts-expect-error: Can't find name MCorp, which is added via <script> in index.html
     // eslint-disable-next-line
-    const mCorpApp = MCorp.app(VUE_APP_MCORP_API_KEY, { anon: true })
+    const mCorpApp = MCorp.app(process.env.VUE_APP_MCORP_API_KEY, { anon: true })
     mCorpApp.run = () => {
       const motion = reactive(mCorpApp.motions['shared'])
       setMotionRef(motion)
