bdcd96b1b9b8ad746a5580ca16276eaa3b288514
Mathias <mbind@protonmail.com>
2024-01-17 09:35:19 +0100

    chore: cleanup activePerformanceService.ts

--- server/services/activePerformanceService.ts
+++ server/services/activePerformanceService.ts
@@ -5,10 +5,9 @@ export const activePerformances: ActivePerformance[] = []
 /**
  * Initializes an active performance if it doesn't already exist. Returns said performance.
  * @param {string} performanceId - ID of the performance.
- * @return {ActivePerformance} The active performance.
+ * @return The active performance.
  */
 export const initializeActivePerformance = (performanceId: string) => {
-  // Check if performance already exists
   let activePerformance = activePerformances.find((p) => p.id === performanceId)
   if (!activePerformance) {
     activePerformance = new ActivePerformance(performanceId)
