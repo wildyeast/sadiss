1580ccf7d9e592f0aa3c598938c1f32b828f81c9
Mathias <mbind@protonmail.com>
2024-01-26 16:19:49 +0100

    set correct log level depending on node_env

--- server/tools/logger.ts
+++ server/tools/logger.ts
@@ -5,8 +5,13 @@ const myFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} ${level}: ${message}`
 })
 
+let desiredLogLevel = 'info'
+if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+  desiredLogLevel = 'debug'
+}
+
 export const logger = createLogger({
-  level: process.env.NODE_ENV === 'test' ? 'debug' : 'info',
+  level: desiredLogLevel,
   format: combine(timestamp(), colorize(), myFormat),
   transports: [new transports.Console()]
 })
