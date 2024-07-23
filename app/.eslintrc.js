d8673ecae0964410085030c6c3a4fb5ed555a4f8
Mathias <mbind@protonmail.com>
2024-07-23 12:53:23 +0200

    feat(vite migration): finish basic migration

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,13 +1,10 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2022: true
   },
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -15,14 +12,11 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-deprecated-slot-attribute': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
--- app/src/composables/useMCorp.ts
+++ app/src/composables/useMCorp.ts
@@ -7,7 +7,7 @@ export function useMCorp() {
   const initializeMCorp = async () => {
     // @ts-expect-error: Can't find name MCorp, which is added via <script> in index.html
     // eslint-disable-next-line
-    const mCorpApp = MCorp.app(process.env.VUE_APP_MCORP_API_KEY, { anon: true })
+    const mCorpApp = MCorp.app(import.meta.env.VITE_APP_MCORP_API_KEY, { anon: true })
     mCorpApp.run = () => {
       const motion = reactive(mCorpApp.motions['shared'])
       setMotionRef(motion)
