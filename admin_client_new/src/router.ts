c0250f771f5f8332aa06e7d67ffdb272117dbbce
Mathias <mbind@protonmail.com>
2024-09-19 22:45:24 +0200

    chore(router): auto-format

--- admin_client_new/src/router.ts
+++ admin_client_new/src/router.ts
@@ -2,13 +2,25 @@ import { createRouter, createWebHistory } from "vue-router"
 import DashboardView from "./views/DashboardView.vue"
 import LoginView from "./views/LoginView.vue"
 import PerformancesView from "./views/PerformancesView.vue"
+import PerformanceDetailView from "./views/PerformanceDetailView.vue"
 import TracksView from "./views/TracksView.vue"
 
 const routes = [
-  { path: "/", component: DashboardView },
-  { path: "/login", component: LoginView, meta: { hideNavbar: true } },
-  { path: "/performances", component: PerformancesView },
-  { path: "/tracks", component: TracksView },
+  { path: "/", name: "Dashboard", component: DashboardView },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginView,
+    meta: { hideNavbar: true },
+  },
+  { path: "/performances", name: "Performances", component: PerformancesView },
+  {
+    path: "/performance/:id",
+    name: "PerformanceDetail",
+    component: PerformanceDetailView,
+    props: true,
+  },
+  { path: "/tracks", name: "Tracks", component: TracksView },
 ]
 
 const router = createRouter({
