2871484c02de2769df9f4979639b23b8e4755ac5
Mathias <mbind@protonmail.com>
2024-09-20 14:41:56 +0200

    feat(auth): add isUserLoggedIn function and update router navigation guards

--- admin_client_new/src/api/authService.ts
+++ admin_client_new/src/api/authService.ts
@@ -17,3 +17,21 @@ export const login = async (email: string, password: string) => {
     }
   }
 }
+
+export async function isUserLoggedIn() {
+  try {
+    const response = await apiClient.get<{
+      message: string
+      user?: { username: string }
+    }>("/is-logged-in")
+    return response.data.user
+  } catch (error: any) {
+    if (error.response) {
+      throw new Error(error.response.data.message || "Login failed")
+    } else if (error.request) {
+      throw new Error("No response from server. Please try again later.")
+    } else {
+      throw new Error("An error occurred. Please try again later.")
+    }
+  }
+}
--- admin_client_new/src/router.ts
+++ admin_client_new/src/router.ts
@@ -4,6 +4,7 @@ import LoginView from "./views/LoginView.vue"
 import PerformancesView from "./views/PerformancesView.vue"
 import PerformanceDetailView from "./views/PerformanceDetailView.vue"
 import TracksView from "./views/TracksView.vue"
+import { isUserLoggedIn } from "./api/authService"
 
 const routes = [
   { path: "/", name: "Dashboard", component: DashboardView },
@@ -13,19 +14,50 @@ const routes = [
     component: LoginView,
     meta: { hideNavbar: true },
   },
-  { path: "/performances", name: "Performances", component: PerformancesView },
+  {
+    path: "/performances",
+    name: "Performances",
+    component: PerformancesView,
+    meta: { requiresAuth: true },
+  },
   {
     path: "/performance/:id",
     name: "PerformanceDetail",
     component: PerformanceDetailView,
+    meta: { requiresAuth: true },
     props: true,
   },
-  { path: "/tracks", name: "Tracks", component: TracksView },
+  {
+    path: "/tracks",
+    name: "Tracks",
+    component: TracksView,
+    meta: { requiresAuth: true },
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach(async (to, _from, next) => {
+  const user = await isUserLoggedIn()
+  // if (user) {
+  //   const store = useStore()
+  //   store.userName = user!.username
+  // }
+
+  if (to.matched.some(record => record.meta.requiresAuth) && !user) {
+    console.log("User not logged in")
+    next("/login")
+  } else {
+    console.log("User already logged in")
+    if (to.name === "Login" && user) {
+      next("/dashboard")
+    } else {
+      next()
+    }
+  }
 })
 
 export default router
