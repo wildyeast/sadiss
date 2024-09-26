bddb9127df5e0a3e046da97143659f4c347ec533
Mathias <mbind@protonmail.com>
2024-09-26 16:17:35 +0200

    feat: update base url

--- admin_client/vite.config.ts
+++ admin_client/vite.config.ts
@@ -4,7 +4,7 @@ import svgLoader from "vite-svg-loader"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/admin-next",
+  base: "/admin",
   plugins: [
     vue(),
     svgLoader({
