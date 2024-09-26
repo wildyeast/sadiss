cc65ed9e4b57ef330e4453d397414b5485bf836a
Mathias <mbind@protonmail.com>
2024-09-26 10:34:08 +0200

    refactor(ui): update top bar layout in App.vue

--- admin_client_new/src/App.vue
+++ admin_client_new/src/App.vue
@@ -17,24 +17,25 @@ onMounted(() => {
 
 <template>
   <!-- Top Bar -->
-  <div class="h-[130px] flex flex-col items-center justify-center">
-    <RouterLink to="/" class="my-[30px]">
+  <div
+    class="h-[130px] md:h-[100px] flex flex-col md:flex-row items-center md:items-start justify-center">
+    <RouterLink to="/" class="my-[30px] md:mt-[20px] md:ml-[60px]">
       <SadissLogo />
     </RouterLink>
     <nav
       v-if="!route.meta.hideNavbar"
-      class="text-sm grid grid-cols-[1fr_1fr_1fr] w-full text-secondary">
+      class="text-sm grid grid-cols-[1fr_1fr_1fr] md:flex md:justify-end md:h-full md:items-center w-full text-secondary">
       <!-- Spacer for left column of grid -->
       <div />
 
       <!-- Main links -->
-      <div class="flex gap-8">
+      <div class="flex gap-8 md:gap-4">
         <RouterLink to="/performances">Performances</RouterLink>
         <RouterLink to="/tracks">Tracks</RouterLink>
       </div>
 
       <!-- User link -->
-      <div class="flex justify-center pr-4">
+      <div class="flex justify-center pr-4 md:pl-8">
         <RouterLink to="/user" class="flex gap-2">
           <span>Usr</span>
           <IconUser />
