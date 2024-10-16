d43d1ae4d36a133ab674ba37b4d0647f7f267b61
Mathias <mbind@protonmail.com>
2024-10-16 14:05:08 +0200

    fix(auth): stop server from crashing because of accessing wrong property on user object

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -64,9 +64,7 @@ exports.isLoggedIn = async (req: Request, res: Response) => {
       if (err) return res.sendStatus(403)
       req.user = user
 
-      const userDocument = await User.findById(req.user!._id, '_id')
-
-      res.json({ message: 'Logged in', userId: userDocument!._id })
+      res.json({ message: 'Logged in', userId: user.id })
     })
   } catch (err) {
     res.status(500).json({ message: 'Failed to check login status' })
