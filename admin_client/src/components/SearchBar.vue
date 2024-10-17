bc3ff18e26e689cc00885e1afd1f8d77e7582d9f
Mathias <mbind@protonmail.com>
2024-10-17 16:18:26 +0200

    feat(SearchBar): add button for clearing search term

--- admin_client/src/components/SearchBar.vue
+++ admin_client/src/components/SearchBar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n"
-import { onUnmounted, ref, watch } from "vue"
+import { onUnmounted, ref, useTemplateRef, watch } from "vue"
 import IconSearch from "../assets/search.svg"
+import IconCross from "../assets/cross.svg"
 
 const emit = defineEmits<{
   (e: "search", query: string): void
@@ -16,6 +17,13 @@ const onInput = (event: Event) => {
   searchQuery.value = target.value
 }
 
+const inputRef = useTemplateRef<HTMLInputElement>("inputRef")
+
+const onClear = () => {
+  searchQuery.value = ""
+  inputRef.value?.focus()
+}
+
 watch(searchQuery, newVal => {
   emit("search", newVal)
 })
@@ -27,13 +35,17 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div class="border border-secondary flex items-center pr-2">
+  <div class="border border-secondary flex items-center pr-2 w-[260px]">
     <input
+      ref="inputRef"
       type="text"
       v-model="searchQuery"
       @input="onInput"
       :placeholder="t('search')"
-      class="bg-white border-none h-[40px]" />
-    <IconSearch />
+      class="bg-white border-none h-[40px] mr-2" />
+    <IconSearch v-if="!searchQuery" />
+    <button v-else @click="onClear">
+      <IconCross class="w-4 h-4" />
+    </button>
   </div>
 </template>
